204a30a65704ba3fc5a49ea35d6874b25e657e14
Jeff Cleverley <jeff@jeffcleverley.com>
2019-03-07 15:07:23 +0800

    first commit

---
+++ mu-gridpane-disable-xmlrpc.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * GridPane Disable XMLRPC
+ *
+ * @package         GridPane\GridPaneDisableXmlrpc
+ * @since           0.1.0
+ * @author          Jeff Cleverley
+ * @license         GNU-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name:     GridPane Disable XMLRPC
+ * Plugin URI:      https://gridpane.com
+ * Description:     Disable XMLRPC to mitigate DDoS
+ * Version:         0.1.0
+ * Author:          Jeff Cleverley
+ * Author URI:      http://github.com/JeffCleverley
+ * Author URI:      https://github.com/JeffCleverley/KillXmlrpc
+ * License:         GPL-2.0+
+ * License URI:     http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+require __DIR__ . '/gridpane-disable-xmlrpc/gridpane-disable-xmlrpc.php';
